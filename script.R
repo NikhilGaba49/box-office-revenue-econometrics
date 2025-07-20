90fa48963cd89f4cbb8452952a07d094b4c5cf1e
Nikhil Gaba <nikhilkumargaba@gmail.com>
2025-07-20 23:46:31 +1000

    Added descriptive statistics in R

--- script.R
+++ script.R
@@ -1,1 +1,10 @@
+# First,  we need to load the data and packages.
+library(stargazer)
+data=read.csv(file="movie_data.csv")
 
+
+# Summary statistics for number of observations, mean, standard deviation, median, and the interquartile range
+stargazer(data, 
+          summary.stat = c("n", "mean", "sd", "median", "p25", "p75"),  
+          type="text", title="Descriptive Statistics",
+          out="sumstats.txt")
